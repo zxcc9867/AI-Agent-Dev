8218f96015b0284c33f14a6b11c62e37d0be8a4a
wonjin <zxcc9867@naver.com>
2025-01-06 23:25:56 +0900

    context를 통해 프로퍼티 드릴링 해결 및 컨텍스트 분리로 특정 프로퍼티가 리렌더링 되지 않도록 설정

---
+++ react-next/section11/README.md
@@ -0,0 +1,3 @@
+## TO DO LIST ! 
+
+<img src="https://github.com/user-attachments/assets/078dfd83-f418-4aad-befb-d658cca9dd83" width=800>
---
+++ react-next/section11/components/Editor.jsx
@@ -0,0 +1,40 @@
+import "./Editor.css";
+import { useState, useRef, useContext } from "react";
+import { TodoDispatchContext } from "../src/App";
+const Editor = () => {
+  const { onCreate } = useContext(TodoDispatchContext); // TodoContext안에서 onCreate를 구조분해할당으로 불러옴.
+
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+  const onChange = (e) => {
+    setContent(e.target.value);
+  };
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return; // 빈문자열인 상태로, 추가 버튼을 누르게되면, 그냥 바로 return을 하여, onCreate함수를 호출하지 않는다.
+    }
+    onCreate(content);
+    setContent(""); // 값을 입력하고, 추가버튼을 누르면, 기존 입력필드에 입력한 값이 사라진다.
+    // 234를 입력하고, 추가 버튼을 누르면, 234가 계속 입력 필드에 남아있음.
+  };
+  return (
+    <div className="Editor">
+      <input
+        onKeyDown={onKeyDown}
+        ref={contentRef}
+        value={content}
+        placeholder="Add New Todo..."
+        onChange={onChange}
+      ></input>
+      <button onClick={onSubmit}>Add</button>
+    </div>
+  );
+};
+
+export default Editor;
