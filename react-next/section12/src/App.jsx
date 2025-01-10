c9957c66a9041cb0ad5fe2655c217a5ad11bd6a7
wonjin <zxcc9867@naver.com>
2025-01-10 23:52:38 +0900

    add image, pont and dynamic routing

--- react-next/section12/src/App.jsx
+++ react-next/section12/src/App.jsx
@@ -3,20 +3,28 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
-import { Routes, Route, Link,useNavigate } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { getEmotionImage } from "./util/get-emtion-images";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2/ " /new" : 새로운 일기를 작성하는 new  페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 
 function App() {
-  const nav = useNavigate()
+  const nav = useNavigate();
   const onClickButton = () => {
-    nav("/new") // 이동하고자 하는 경로를 넣는다
-  }
+    nav("/new"); // 이동하고자 하는 경로를 넣는다
+  };
   return (
     // Routes 외부에 컴포넌트를 배치하면, 루트와 관계없이 무조건 렌더링이 됨
     <>
+      <div>
+        <img src={getEmotionImage(1)}></img>
+        <img src={getEmotionImage(2)}></img>
+        <img src={getEmotionImage(3)}></img>
+        <img src={getEmotionImage(4)}></img>
+        <img src={getEmotionImage(5)}></img>
+      </div>
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
@@ -26,8 +34,8 @@ function App() {
       <button onClick={onClickButton}>페이지 이동</button>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/diary" element={<Diary />} />
+        <Route path="/new:id" element={<New />} />
+        <Route path="/diary/:id/:title" element={<Diary />} />
         <Route path="*" element={<Notfound />}></Route>
       </Routes>
     </>
--- react-next/section12/src/pages/Diary.jsx
+++ react-next/section12/src/pages/Diary.jsx
@@ -1,5 +1,15 @@
+import { useParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 const Diary = () => {
-  return <div>Diary</div>;
+  const params = useParams();
+  const [value, setvalue] = useSearchParams(); // 쿼리 스트링 하는 방법
+  console.log(params); // { id: '123' }
+  console.log(value.get("value"),value.get("test")); // { search: '?title=hello&content=world' }
+  return (
+    <div>
+      {params.id}번째 일기입니다. 제목은{params.title}
+    </div>
+  );
 };
 
 export default Diary;
